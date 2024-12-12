f16ad2b1ecef6019d5922967959f953328e1d52e
AMaslyakov <a.v.masliakov@urfu.me>
2024-12-13 02:06:32 +0500

    Solved Task 2100 from Timus

---
+++ Solution2100.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution2100 {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int invites = input.nextInt();
+    String guest;
+    int guest_count = invites + 2;
+    for (int i = 0; i < invites; i++) {
+      guest = input.next();
+      if (guest.contains("+one")) {
+        guest_count++;
+      }}
+    if(guest_count == 13){guest_count++;}
+    System.out.println(guest_count*100);
+  }}
+
