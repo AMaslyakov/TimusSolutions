d7c1767bddf7770c225cf37ad9e2c352fa7f7a65
AMaslyakov <a.v.masliakov@urfu.me>
2024-12-15 23:25:17 +0500

    Solved task 1785 from Timus

---
+++ src/Solution1785.java
@@ -0,0 +1,47 @@
+
+import java.io.*;
+
+public  class Solution1785 {
+    public static void main(String[] args) throws IOException {
+       new Solution1785().run();
+    }
+
+    StreamTokenizer input;
+    PrintWriter output;
+
+    void run() throws IOException {
+        input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        output = new PrintWriter(System.out);
+        solve();
+        output.flush();}
+
+    int nextInt() throws IOException{
+        input.nextToken();
+        return  (int) input.nval;
+    }
+
+    void solve() throws IOException {
+            int N = nextInt();
+            String result = "few";
+            if(N>=1000){
+               result = "legion"; 
+            } else if (N>=500) {
+                result = "zounds";
+            } else if (N>=250) {
+                result = "swarm";}
+            else if (N>=100){
+                result = "throng";}
+            else if (N>=50){
+                result = "horde";}
+            else if (N>=20) {
+                result = "lots";}
+            else if (N>=10) {
+               result = "pack";}
+            else if (N>=5){
+                result = "several";}
+        else { result = "few";}
+
+            output.print(result);
+            output.print("\n");
+        }
+    }
