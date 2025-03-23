c300de937622e0dc6e02194bd79e330a8f1e66b0
AMaslyakov <a.v.masliakov@urfu.me>
2025-03-23 22:44:20 +0500

    Solved task 1209 from Timus Online Judge

---
+++ src/semestr2/Solution1209.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Solution1209{
+    public static void main(String[] args){Scanner input = new Scanner(System.in);
+      int n = input.nextInt();
+      ArrayList<Boolean> r = new ArrayList<>();
+      for(int i=1; i<=n; i++){
+        int index = input.nextInt();
+        r.add(find_k(index));
+      }
+      
+      for(Boolean e:r){
+        System.out.print((e ? 1 : 0) + " ");
+      }
+      System.out.println();
+
+    }
+
+    static boolean find_k(int n){
+      
+     return (1+Math.sqrt(8*(double)n - 7))%2 == 0;
+    }
+}
