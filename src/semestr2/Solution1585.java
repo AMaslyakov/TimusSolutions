f07082e7ba8a5923b3fc538064e6af41dc24acff
AMaslyakov <a.v.masliakov@urfu.me>
2025-03-24 00:12:41 +0500

    Solved task 1585 from timus online judge

---
+++ src/semestr2/Solution1585.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+
+
+public class Solution1585{
+    public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+      int n = input.nextInt();
+
+      Map<String, Integer> pinguins = new HashMap<>();
+      //Emperor Penguin», «Little Penguin» и «Macaroni Penguin»
+      pinguins.put("Emperor Penguin",0);
+      pinguins.put("Little Penguin",0);
+      pinguins.put("Macaroni Penguin",0);
+
+      input.nextLine();
+
+      for(int i =0; i<n; i++){
+         String species = input.nextLine();
+         pinguins.compute(species, (k,v)->v+1);
+      }
+
+
+      Map.Entry<String, Integer> m = Collections.max(
+          pinguins.entrySet(),
+          Map.Entry.comparingByValue()
+          );
+
+        System.out.println(m.getKey());
+    }
+}
