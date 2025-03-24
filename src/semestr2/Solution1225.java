93a075219a7db90667cc09938946c04f3ca91d0a
AMaslyakov <a.v.masliakov@urfu.me>
2025-03-24 06:30:48 +0500

    Solved task 1225 from TOJ

---
+++ src/semestr2/Solution1225.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution1225 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        input.close();
+
+        long[] dp = new long[N + 2];
+
+        dp[0] = 2; 
+        dp[1] = 2; 
+
+        for (int i = 2; i < N+2; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.println(dp[N-1]);
+    }
+}
+
